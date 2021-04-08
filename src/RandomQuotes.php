df1d072ffad23de51fd9210a62980e8c1f4b8830
andrijaitisTom <andrijaitis.tom@gmail.com>
2021-04-08 11:33:27 +0300

    compose random quotes class

---
+++ src/RandomQuotes.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RandomQuotes;
+
+class RandomQuotes {
+
+public function getRandomQuotes() {
+    $quotes = file_get_contents( __DIR__.'/../assets/quotes.json' );
+
+    $quotes = json_decode($quotes, true);
+
+    $index = mt_rand (0, count($quotes));
+
+    return $quotes[$index];
+
+    return $quotes;
+}
+
+    
+    public function generate(){
+    return $this->getRandomQuotes();
+    }
+}
